a923786f0b0885ad294282db653d434b4c8c1a13
aryamanarora <aryaman.arora2020@gmail.com>
2018-10-29 11:30:50 -0400

    works

--- app.py
+++ app.py
@@ -1,7 +1,55 @@
 from flask import Flask, request, json, Response, redirect, url_for, render_template
+from wtforms import Form, BooleanField, StringField, PasswordField, IntegerField, FloatField, validators
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+diabetes = pd.read_csv('diabetes.csv')
+del diabetes['DiabetesPedigreeFunction']
+print(diabetes.info())
+
+knn = None
+X_train, X_test, y_train, y_test = train_test_split(diabetes.loc[:, diabetes.columns != 'Outcome'], diabetes['Outcome'], stratify=diabetes['Outcome'], random_state=66)
+
+neighbors_settings = range(1, 11)
+
+for n_neighbors in neighbors_settings:
+    # build the model
+    knn = KNeighborsClassifier(n_neighbors=n_neighbors)
+    knn.fit(X_train, y_train)
 
 app = Flask(__name__)
 
-@app.route('/')
+class DiabetesForm(Form):
+    age = IntegerField('Age')
+    bmi = FloatField('BMI')
+    bp = IntegerField('Diastolic blood pressure (mm Hg)')
+    glucose = IntegerField('Glucose (plasma glucose concentration over 2 hours)')
+    insulin = IntegerField('2 hour serum insulin (mu U/mL)')
+    skin = IntegerField('Skin thickness (triceps skin fold thickness in mm)')
+    pregnancies = IntegerField('Number of pregnancies')
+    accept_tos = BooleanField('I will not substitute this for a doctor\'s assessment', [validators.DataRequired()])
+
+@app.route('/', methods=['GET', 'POST'])
 def main():
-    return render_template("index.html")
+    global knn
+    form = DiabetesForm(request.form)
+    if request.method == 'POST' and form.validate():
+        res = knn.predict_proba(
+            [[
+                float(form.pregnancies.data),
+                float(form.glucose.data),
+                float(form.bp.data),
+                float(form.skin.data),
+                float(form.insulin.data),
+                float(form.bmi.data),
+                float(form.age.data)
+            ]]
+        )
+        for i in res.tolist():
+            print(i)
+        return render_template("result.html", res=int(res[0][0] * 100))
+        
+    return render_template("index.html", form=form)
--- test.py
+++ test.py
@@ -5,37 +5,37 @@ import numpy as np
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
-
+from sklearn.ensemble import GradientBoostingClassifier
 
 #Get Data
-filePath = "bigyeet.csv"
+filePath = "Diabetes.csv"
 Data = pd.read_csv(filePath)
+del Data['DiabetesPedigreeFunction']
 print(Data.columns)
+X_train, X_test, y_train, y_test = train_test_split(Data.loc[:, Data.columns != 'Outcome'], Data['Outcome'], stratify=Data['Outcome'], random_state=66, test_size = 0.25)
 
-target = ["age","sex","cp","trestbps","chol","fbs","restecg","thalach","exang","oldpeak","slope","ca","thal","num"]
 
-#Set target and predictors
-X = Data.columns
-Y = Data[target]
-print("DESCRIBE DATA")
-print(Y.describe())
 
-#Split into training and test data
-trainX, testX, trainY, testY = train_test_split(X, Y, test_size=0.25, random_state=2)
+
+
 
 #rs 2 = 14.6305
 #Calculate accuracy
 
-def getMae(randomState, maxDepth, minSamplesSplit, minImpurityDecrease):
+def getMae():
     # Make model
-    Jarvis = RandomForestRegressor(random_state=120, max_depth=3, min_samples_split=7, min_impurity_decrease=40)
-    Jarvis.fit(trainX, trainY)
+    Jarvis = GradientBoostingClassifier(random_state = 6, min_samples_split = 2, max_leaf_nodes = 15, max_features = 7)
+    Jarvis.fit(X_train, y_train)
     # Predict
-    prediction = Jarvis.predict(testX)
-    answers = testY
+    prediction = Jarvis.predict(X_test)
+    answers = y_test
     #Calculate mean absolute error
     mae = mean_absolute_error(answers, prediction)
     return mae
+print(getMae())
 
-print(getMae(0, 0, 0, 0))
+for i in range(1, 100):
+    if(getMae(i) < 0.3066):
+        print(getMae(i))
+        print(i)
 
